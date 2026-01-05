5bc0ca2fa41985642e22c596ef799dd7597d269c
Rémi Campistron <remi.campistron@gmail.com>
2026-01-05 15:13:33 +0100

    Créer la classe de gestion des exceptions sql

---
+++ src/coursesshop/dao/DaoException.java
@@ -0,0 +1,10 @@
+package coursesshop.dao;
+
+public class DaoException extends RuntimeException {
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public DaoException(String message) {
+        super(message);
+    }
+}

c7592072f49d9c99a83b43f3a5828f8dd85258df
Rémi Campistron <remi.campistron@gmail.com>
2026-01-06 14:15:58 +0100

    Ajout de la methode listcategories dans CourseBuyingService

--- src/coursesshop/business/CourseBuyingService.java
+++ src/coursesshop/business/CourseBuyingService.java
@@ -1,6 +1,8 @@
 package coursesshop.business;
 
+import coursesshop.dao.CategoryDao;
 import coursesshop.dao.CourseDao;
+import coursesshop.model.Category;
 import coursesshop.model.Course;
 
 import java.util.List;
@@ -9,5 +11,10 @@ public class CourseBuyingService {
     public List<Course> listCourses(){
         CourseDao coursDao = new CourseDao();
         return coursDao.findAll();
+    }
+
+    public List<Category> listCategories(){
+        CategoryDao categoryDao = new CategoryDao();
+        return categoryDao.findAll();
     }
 }

ba1b2e186a0793dedfe09ec07fa98f50c1ae0714
Rémi Campistron <remi.campistron@gmail.com>
2026-01-07 13:33:07 +0100

    Ajout de setters dans category

--- src/coursesshop/model/Category.java
+++ src/coursesshop/model/Category.java
@@ -1,18 +1,28 @@
 package coursesshop.model;
 
 public class Category {
-    private int id;
-    private String name;
 
-    public Category(int id,String name){
-        this.id = id;
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
-    public int getId() {
-        return id;
-    }
+  private int id;
+  private String name;
 
+  public Category(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }

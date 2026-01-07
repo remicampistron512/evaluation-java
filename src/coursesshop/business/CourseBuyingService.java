77556513743e1002ede69cf18062651431507b38
Rémi Campistron <remi.campistron@gmail.com>
2026-01-07 08:59:32 +0100

    Impélemente les modes de suivi de formation dans CourseBuyingService

--- src/coursesshop/business/CourseBuyingService.java
+++ src/coursesshop/business/CourseBuyingService.java
@@ -1,9 +1,11 @@
 package coursesshop.business;
 
+import coursesshop.dao.AttendanceModeDao;
 import coursesshop.dao.CategoryDao;
 import coursesshop.dao.CourseDao;
 import coursesshop.model.Category;
 import coursesshop.model.Course;
+import coursesshop.model.enums.AttendanceMode;
 
 import java.util.List;
 
@@ -26,5 +28,15 @@ public class CourseBuyingService {
     public List<Course> listCoursesByKeyword(String keyword) {
         CourseDao courseDao = new CourseDao();
         return courseDao.findByKeyword(keyword);
+    }
+
+    public List<AttendanceMode> listAttendanceModes() {
+        AttendanceModeDao attendanceModeDao = new AttendanceModeDao();
+        return attendanceModeDao.listAttendanceModeCodes();
+    }
+
+    public List<Course> listCoursesByAttendance(AttendanceMode attendanceMode) {
+        CourseDao courseDao = new CourseDao();
+        return courseDao.findByAttendance(attendanceMode);
     }
 }

32de0d8f40dcac88bc316b8fe0562bbe5f135c08
Rémi Campistron <remi.campistron@gmail.com>
2026-01-07 13:17:46 +0100

    Passage des propriétés en private

--- src/coursesshop/model/Course.java
+++ src/coursesshop/model/Course.java
@@ -1,77 +1,75 @@
 package coursesshop.model;
+
 import coursesshop.model.enums.AttendanceMode;
 import java.math.BigDecimal;
 
 public class Course {
-    protected int id;
-    protected String name;
-    protected String description;
-    protected int durationDays;
-    protected BigDecimal price;
-    protected AttendanceMode mode;
+
+  private int id;
+  private String name;
+  private String description;
+  private int durationDays;
+  private BigDecimal price;
+  private AttendanceMode mode;
 
 
-    public int getId() {
-        return id;
-    }
+  public Course(int id, String name, String description, int durationDays, BigDecimal price,
+      AttendanceMode mode) {
+    this.id = id;
+    this.name = name;
+    this.durationDays = durationDays;
+    this.description = description;
+    this.price = price;
+    this.mode = mode;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public int getDurationDays() {
-        return durationDays;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
+  public int getDurationDays() {
+    return durationDays;
+  }
 
+  public void setDurationDays(int durationDays) {
+    this.durationDays = durationDays;
+  }
 
-    public void setDurationDays(int durationDays) {
-        this.durationDays = durationDays;
-    }
+  public BigDecimal getPrice() {
+    return price;
+  }
 
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
 
+  public AttendanceMode getMode() {
+    return mode;
+  }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public AttendanceMode getMode() {
-        return mode;
-    }
-
-    public void setMode(AttendanceMode mode) {
-        this.mode = mode;
-    }
-
-
-    public Course(int id, String name,  String description,int durationDays, BigDecimal price, AttendanceMode mode) {
-        this.id = id;
-        this.name = name;
-        this.durationDays = durationDays;
-        this.description = description;
-        this.price = price;
-        this.mode = mode;
-    }
+  public void setMode(AttendanceMode mode) {
+    this.mode = mode;
+  }
 
 
 }

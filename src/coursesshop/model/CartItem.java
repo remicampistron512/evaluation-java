be7813fda65b48a62aa02ae3e2e8a847e91c70dd
Rémi Campistron <remi.campistron@gmail.com>
2026-01-09 15:56:27 +0100

    création de la classe cartitem

--- src/coursesshop/model/CartItem.java
+++ src/coursesshop/model/CartItem.java
@@ -3,37 +3,29 @@ package coursesshop.model;
 import java.math.BigDecimal;
 
 public class CartItem {
-  private int id;
-  private int quantity;
-  private BigDecimal unitPrice;
+  private int id;          // cit_id
+  private int courseId;    // cou_id
+  private int quantity;    // cit_quantity
+  private BigDecimal unitPrice; // cit_unit_price
 
-  public CartItem(int id, int quantity, BigDecimal unitPrice) {
+  public CartItem() {}
+
+  public CartItem(int id, int courseId, int quantity, BigDecimal unitPrice) {
     this.id = id;
+    this.courseId = courseId;
     this.quantity = quantity;
     this.unitPrice = unitPrice;
   }
 
-  public int getId() {
-    return id;
-  }
+  public int getId() { return id; }
+  public void setId(int id) { this.id = id; }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+  public int getCourseId() { return courseId; }
+  public void setCourseId(int courseId) { this.courseId = courseId; }
 
-  public int getQuantity() {
-    return quantity;
-  }
+  public int getQuantity() { return quantity; }
+  public void setQuantity(int quantity) { this.quantity = quantity; }
 
-  public void setQuantity(int quantity) {
-    this.quantity = quantity;
-  }
-
-  public BigDecimal getUnitPrice() {
-    return unitPrice;
-  }
-
-  public void setUnitPrice(BigDecimal unitPrice) {
-    this.unitPrice = unitPrice;
-  }
+  public BigDecimal getUnitPrice() { return unitPrice; }
+  public void setUnitPrice(BigDecimal unitPrice) { this.unitPrice = unitPrice; }
 }
